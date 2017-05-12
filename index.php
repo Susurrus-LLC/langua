c2682be2bb4a53b5bd8e1546a740507f51e246c1
Ian A. Cook <nai888@gmail.com>
2017-05-12 12:59:58 -0500

    Fixed typo

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php include("components/header.php"); ?>
 <div class="home">
-	<p>Langua is a suite of language tools that can be used for testing, experiemntation, learning, analysis, or creation. There are four main tools in the suite.</p>
+	<p>Langua is a suite of language tools that can be used for testing, experimentation, learning, analysis, or creation. There are four main tools in the suite.</p>
 	<h2>Gen</h2>
 	<p>LanguaGen is a tool for automatically building a set of words based on arbitrary rules of phonotactics.</p>
 	<h2>Morph</h2>
-	<p>LanguaMorph is a tool for modeling historical sound change according to arbitrary rules of phonetic change. It can also be used to reverse historical sound change according to those same rules in order to guess at a word’s proto-language roots.</p>
+	<p>LanguaMorph is a tool for modeling historical sound change according to arbitrary rules of phonetic change. It can also be used to reverse historical sound change according to those same rules in order to guess at a word&rsquo;s proto-language roots.</p>
 	<h2>Deriv</h2>
 	<p>LanguaDeriv is a tool for modeling word derivation according to arbitrary rules of morphosyntactics.</p>
 	<h2>Frequen</h2>
