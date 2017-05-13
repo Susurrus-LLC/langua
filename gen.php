3e67ab0efc43152ef317888a4589c8a8b66db3c7
Ian A. Cook <nai888@gmail.com>
2017-05-12 23:01:21 -0500

    Start framework for Gen page

--- gen.php
+++ gen.php
@@ -1,5 +1,19 @@
 <?php include("components/header.php"); ?>
 <div class="gen">
+	<h2 class="tool-title">LanguaGen</h2>
 	<p class="notice">This tool is still under construction.</p>
+	<div class="input">
+		<div class="subpatterns">
+			
+		</div>
+		<div class="pattern">
+
+		</div>
+		<div class="control">
+
+		</div>
+	</div>
+	<div class="results">
+	</div>
 </div>
 <?php include("components/footer.php"); ?>
