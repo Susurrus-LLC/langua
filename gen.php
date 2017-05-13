e9dbdf0f603e31c1b88b2cc605d10062f3c44d3c
Ian A. Cook <nai888@gmail.com>
2017-05-13 10:21:27 -0500

    Starting to build subpattern rows

--- gen.php
+++ gen.php
@@ -4,7 +4,10 @@
 	<p class="notice">This tool is still under construction.</p>
 	<div class="input">
 		<div class="subpatterns">
-			
+			<select name="sub1" class="permanent" data-sel="V"></select>
+			<select name="sub2" class="permanent" data-sel="C"></select>
+			<select name="sub3" class="permanent" data-sel="N"></select>
+			<select name="sub4" class="permanent" data-sel="null"></select>
 		</div>
 		<div class="pattern">
 
--- scripts/gen.js
+++ scripts/gen.js
@@ -1,1 +1,57 @@
-var genView = '<div class="gen"><p class="notice">This tool is still under development</p></div>';
+var subpatterns = function () {
+	var self = this;
+	this.options = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	this.used = [];
+	this.remaining = this.options;
+	this.subNum = 4;
+	this.addSub = function (selection, num) {
+		var $sub = num !== null ? $(".sub" + num) : $(".sub" + self.subNum);
+		var noneSel = selection !== null ? "" : " selected";
+		console.log($sub);
+		console.log(noneSel);
+		$sub.html('<option value="-"' + selected + '>-</option>');
+		for (var i = 0; i < self.remaining.length; i++) {
+			var val = self.remaining[i];
+			if (self.remaining[i] === selection) {
+				$sub.append('<option value="' + selection + '" selected>' + selection + '</option>');
+				console.log("append selected");
+			} else {
+				$sub.append('<option value="' + val + '">' + val + '</option>');
+				console.log("append");
+			}
+		}
+		evalUsed();
+		evalRemaining();
+	}
+
+	function evalUsed() {
+		var currUsed = [];
+		for (var i = 0; i <= self.subNum; i++) {
+			var dataSel = $(".sub" + i).attr("data-sel");
+			currUsed.push(dataSel);
+		}
+		currUsed.sort();
+		self.used = currUsed;
+	}
+
+	function evalRemaining() {
+		var currRemaining = self.options;
+		for (var i = 0; i < self.used.length; i++) {
+			var ind = currRemaining[self.used[i]];
+			if (ind > -1) {
+				currRemaining.splice(ind, 1);
+			}
+		}
+		currRemaining.sort();
+		self.remaining = currRemaining;
+	}
+};
+
+$(document).ready(function () {
+	console.error();
+	console.log("ready");
+	subpatterns.addSub("V", 1);
+	subpatterns.addSub("C", 2);
+	subpatterns.addSub("N", 3);
+	subpatterns.addSub();
+});
