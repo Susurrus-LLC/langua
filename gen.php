79113d3abe84fe3dfeb0ed653869a1793d56a238
Ian A. Cook <nai888@gmail.com>
2017-05-18 21:06:26 -0500

    Adding gen subpattern and pattern inputs

--- gen.php
+++ gen.php
@@ -1,11 +1,15 @@
 <?php include("components/header.php"); ?>
 <div class="gen">
 	<h2 class="tool-title">LanguaGen</h2>
-	<p class="notice">This tool is still under construction.</p>
 	<div class="input">
-		<div class="subpatterns"></div>
-		<div class="pattern"></div>
-		<div class="control"></div>
+		<div class="subpatterns part">
+			<p class="section-title">Subpatterns</p>
+		</div>
+		<div class="pattern part">
+			<p class="section-title">Pattern</p>
+			<input type="text" class="pattern-input"></input>
+		</div>
+		<div class="control part"></div>
 	</div>
 	<div class="results">
 	</div>
