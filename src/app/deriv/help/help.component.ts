9170b931162559d055a29cb2e37eb2280d9d1949
Ian A. Cook <nai888@gmail.com>
2017-08-14 18:46:31 -0500

    Update page title on help pages

--- src/app/deriv/help/help.component.ts
+++ src/app/deriv/help/help.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-gen-help',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent {
-  
+  constructor(private titleService: Title) {
+    titleService.setTitle('Langua | Deriv Help')
+  }
 }
--- src/app/frequen/help/help.component.ts
+++ src/app/frequen/help/help.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-frequen-help',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core'
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent {
-  
+  constructor(private titleService: Title) {
+    titleService.setTitle('Langua | Frequen Help')
+  }
 }
