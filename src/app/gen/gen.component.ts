80853b496af8b2c66cc05a8b503699640e2bda20
Ian A. Cook <nai888@gmail.com>
2017-08-13 20:39:00 -0500

    Updating component files

--- src/app/gen/gen.component.ts
+++ src/app/gen/gen.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-gen',
   templateUrl: './gen.component.html',
   styleUrls: ['./gen.component.css']
 })
-export class GenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class GenComponent {
 
 }
