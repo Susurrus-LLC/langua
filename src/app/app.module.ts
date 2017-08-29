912ccf065437356530e87f70b294140d1e470275
Ian A. Cook <nai888@gmail.com>
2017-08-28 21:04:07 -0500

    Trying to dynamically set base href based on env
    Working on setting the base href dynamically such that env.dev will be '/'
    and env.prod will be /langua/.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from '@angular/common'
 import { BrowserModule, Title } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
@@ -6,13 +7,14 @@ import { AppComponent } from './app.component'
 import { FooterComponent } from './footer/footer.component'
 import { HeaderComponent } from './header/header.component'
 import { HomeComponent } from './home/home.component'
+import { NotFoundComponent } from './not-found/not-found.component'
 import { AppRoutingModule } from './app-routing.module'
 import { DerivModule } from './deriv/deriv.module'
 import { FrequenModule } from './frequen/frequen.module'
 import { GenModule } from './gen/gen.module'
 import { MorphModule } from './morph/morph.module'
 import { TreeModule } from './tree/tree.module'
-import { NotFoundComponent } from './not-found/not-found.component'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [
@@ -32,6 +34,7 @@ import { NotFoundComponent } from './not-found/not-found.component'
     TreeModule
   ],
   providers: [
+    { provide: APP_BASE_HREF, useValue: environment.baseUrl },
     Title
   ],
   bootstrap: [AppComponent]
