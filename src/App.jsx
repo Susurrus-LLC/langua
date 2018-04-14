323577c4c59b17ba8ceb6ee609d07d7a9848deba
Ian A. Cook <nai888@users.noreply.github.com>
2018-04-14 10:12:53 -0500

    Minor cleanup
    Fixed deploy command to overwrite gh-pages branch. Shrunk App to
    functional component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className='App'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
 
 export default App
