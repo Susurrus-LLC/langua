8063159ebbceb480dbe9bb3239f13a4dd636e719
Ian A. Cook <nai888@users.noreply.github.com>
2018-04-14 10:58:08 -0500

    Built grid on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,32 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = () => (
   <div className='home'>
-    <h2 class='tool-title'>Home</h2>
-    <p class='notice'>This tool is still in planning.</p>
+    <p>Langua is a suite of language tools that can be used for testing, experimentation, learning, analysis, or creation. There are four main tools in the suite.</p>
+    <div className='grid'>
+      <Link to='/gen'>
+        <h2>Gen</h2>
+        <p>LanguaGen is a tool for automatically building a set of words based on arbitrary rules of phonotactics.</p>
+      </Link>
+      <Link to='/morph'>
+        <h2>Morph</h2>
+        <p>LanguaMorph is a tool for modeling historical sound change according to arbitrary rules of phonetic change. It can also be used to reverse historical sound change according to those same rules in order to guess at a word&rsquo;s proto-language roots.</p>
+      </Link>
+      <Link to='/deriv'>
+        <h2>Deriv</h2>
+        <p>LanguaDeriv is a tool for modeling word derivation according to arbitrary rules of morphosyntactics.</p>
+      </Link>
+      <Link to='/frequen'>
+        <h2>Frequen</h2>
+        <p>LanguaFrequen is a tool for analyzing phoneme frequencies in a given text.</p>
+      </Link>
+      <Link to='/tree'>
+        <h2>Tree</h2>
+        <p>LanguaTree is a tool for drawing and labeling syntax trees.</p>
+      </Link>
+    </div>
   </div>
 )
 
