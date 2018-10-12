f33434a4206a19dc5a2b1bb38c48686f4bb28e7d
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2018-10-12 16:51:31 -0500

    Start building Frequen

--- src/components/Frequen/Frequen.js
+++ src/components/Frequen/Frequen.js
@@ -9,20 +9,46 @@ import ButtonLink from '../Button/ButtonLink'
 
 import { canonical, siteTitle } from '../../App'
 
-const Frequen = (props) => (
-  <div className={props.classes.frequen}>
-    <Helmet>
-      <title>{siteTitle} Frequen</title>
-      <meta name='description' content='LanguaFrequen is a tool for analyzing phoneme frequencies in a given text.' />
-      <link rel='canonical' href={`${canonical}frequen/`} />
-    </Helmet>
-    <ButtonLink route='/frequen/help'>
-      Help
-    </ButtonLink>
-    <h2 className='toolTitle'>LanguaFrequen</h2>
-    <Notice>This tool is still in planning.</Notice>
-  </div>
-)
+class Frequen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      corpus: '',
+      consonants: [''],
+      vowels: ['']
+    }
+  }
+
+  onChangeCorpus (e) {
+    const val = e.target.value
+    this.setState(prevState => ({
+      corpus: val
+    }))
+  }
+
+  render () {
+    return (
+      <div className={this.props.classes.frequen}>
+        <Helmet>
+          <title>{siteTitle} Frequen</title>
+          <meta name='description' content='LanguaFrequen is a tool for analyzing phoneme frequencies in a given text.' />
+          <link rel='canonical' href={`${canonical}frequen/`} />
+        </Helmet>
+        <ButtonLink route='/frequen/help'>
+          Help
+          </ButtonLink>
+        <h2 className='toolTitle'>LanguaFrequen</h2>
+        <Notice>This tool is still in development.</Notice>
+        <h5>Text Corpus</h5>
+        <textarea
+          className={this.props.classes.corpus}
+          id='corpus'
+          name='corpus'
+        />
+      </div>
+    )
+  }
+}
 
 Frequen.propTypes = {
   classes: PropTypes.object
--- src/components/Frequen/styles.js
+++ src/components/Frequen/styles.js
@@ -1,3 +1,10 @@
-const styles = {}
+import * as v from '../../styles/variables'
+
+const styles = {
+  corpus: {
+    width: '100%',
+    height: `${2 * v.ms6}rem`
+  }
+}
 
 export default styles
