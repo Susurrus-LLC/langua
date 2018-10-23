479b40132b9417c3ccf00245ee689435e3f63d21
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2018-10-23 08:41:08 -0500

    Add more text to FrequenHelp

--- src/components/Frequen/Help/FrequenHelp.js
+++ src/components/Frequen/Help/FrequenHelp.js
@@ -28,9 +28,42 @@ const FrequenHelp = props => (
     <h3 id='using'>Using LanguaFrequen</h3>
     <p>
       Input a corpus of text in the <strong>Text Corpus</strong> field. This is
-      the text that will be analyzed. Next, add a list of consonants and vowels
-      accordingly to the <strong>Consonants</strong> and <strong>Vowels</strong>{' '}
-      lists.
+      the text that will be analyzed. The text can be phomic or phonetic, or it
+      can simply use the language&rsquo;s standard spelling system. For best
+      results, however, each sound that should be analyzed separately will need
+      its own grapheme or grapheme cluster. For example, in English, the letter{' '}
+      <code>y</code> would need to be differentiated based on when it occurs as
+      a consonant vs. when it occurs as a vowel. It does not matter if
+      punctuation is removed from the corpus, as any graphemes not identified
+      for analysis in the next step will be ignored by the tool.
+    </p>
+    <p>
+      Next, add a list of consonants and vowels accordingly to the{' '}
+      <strong>Consonants</strong> and <strong>Vowels</strong> lists. Separate
+      each of the segments with a forward slash (<code>/</code>
+      ). These segments can be single graphemes or grapheme clusters, and a
+      cluster can contain characters that are used in shorter segments. For
+      example, given the corpus <code>kanto</code> and the consonants{' '}
+      <code>n/t/nt</code>, the tool will identify one occurance of{' '}
+      <code>nt</code>, but no occurances of <code>n</code> or <code>t</code>.
+      Any graphemes not identified for analysis will be ignored. In the previous
+      example, since <code>k</code> was not added to the list of consonants, the
+      tool would not count its occurance.
+    </p>
+    <p>
+      Allophones can be added after a segment to indicate that multiple segments
+      should all be counted as occurances of the same segment. Separate
+      allophones with a comma (<code>,</code>
+      ). For example, given the corpus <code>potaná</code>, if the vowels list
+      contained <code>a/á/o</code>, the tool would identify one occurance each
+      of <code>a</code>, <code>á</code>, and <code>o</code>, but if the vowels
+      list contained <code>a,á/o</code>, the tool would identify two occurances
+      of <code>a</code> and one occurance of <code>o</code>.
+    </p>
+    <p>
+      When ready, click the <strong>Analyze</strong> button to run the analysis.
+      The tool will draw a graph and display a table showing the frequencies of
+      each of the identified segments in the text corpus.
     </p>
   </div>
 )
