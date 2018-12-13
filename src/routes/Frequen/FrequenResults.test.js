188b49917f354548ffa5e646d495c18422093b3d
Ian A. Cook <6866411+nai888@users.noreply.github.com>
2018-12-12 18:24:01 -0600

    Update Frequen test

--- src/routes/Frequen/FrequenResults.test.js
+++ src/routes/Frequen/FrequenResults.test.js
@@ -2,17 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import FrequenResults from './FrequenResults'
-import { defData } from './defaultData'
 
 describe('FrequenResults', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(
       <FrequenResults
-        results={defData.results}
-        hovered={defData.hovered}
-        filter={defData.filter}
-        analyzed={defData.analyzed}
+        classes={{ classes: 'classes' }}
+        filterSeg='all'
+        analyzed={false}
         onChangeInput={jest.fn()}
         onMouseOver={jest.fn()}
         onMouseOut={jest.fn()}
