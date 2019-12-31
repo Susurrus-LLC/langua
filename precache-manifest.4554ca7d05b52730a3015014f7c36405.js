91d371c3138e33696e32a5f51b6809fd91094979
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-31 00:01:23 +0000

    Deploy nai888/langua to github.com/nai888/langua.git:gh-pages

---
+++ precache-manifest.4554ca7d05b52730a3015014f7c36405.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773203b4ec314884a48459dc00a4fd3d",
+    "url": "/langua/index.html"
+  },
+  {
+    "revision": "61795898872798b90715",
+    "url": "/langua/static/css/2.cc010c4f.chunk.css"
+  },
+  {
+    "revision": "61795898872798b90715",
+    "url": "/langua/static/js/2.152a4c1d.chunk.js"
+  },
+  {
+    "revision": "fcbf3c068d59b169eb6b",
+    "url": "/langua/static/js/main.89342ec9.chunk.js"
+  },
+  {
+    "revision": "11b593aadbb8e32c2fa9",
+    "url": "/langua/static/js/runtime~main.dc493b17.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/langua/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/langua/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/langua/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/langua/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/langua/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/langua/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/langua/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/langua/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/langua/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/langua/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/langua/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/langua/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/langua/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/langua/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/langua/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
