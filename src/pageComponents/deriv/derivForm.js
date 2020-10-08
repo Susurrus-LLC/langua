de05f148958d11b38c2ccc1625a2a8a7f859e088
Ian Cook <6866411+nai888@users.noreply.github.com>
2020-10-08 18:20:02 -0500

    Build Deriv

--- src/pageComponents/deriv/derivForm.js
+++ src/pageComponents/deriv/derivForm.js
@@ -1,14 +1,227 @@
 import React from 'react'
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
 
 import Form from '../../components/form'
+import Button from '../../components/button'
+import Control from '../../components/control'
+import ControlSide from '../../components/control/side'
+import ControlPiece from '../../components/control/piece'
 
 import sharedStyles from '../../components/form/sharedForm.module.sass'
 
-const DerivForm = () => <Form name='deriv-form' />
+const DerivForm = ({ styles, data, change }) => {
+  const filePicker = React.createRef()
+
+  const lexemeRows = data.lexemes.map((lexeme, index) => (
+    <div key={index} className={styles.derivRow}>
+      <input
+        type='text'
+        id={`lw${index}`}
+        name={`lw${index}`}
+        className={styles.lexemeInput}
+        value={lexeme.lexeme}
+        onChange={change}
+        aria-label={`Word for lexeme ${index}`}
+      />
+      <input
+        type='text'
+        id={`ld${index}`}
+        name={`ld${index}`}
+        className={styles.lexemeDefInput}
+        value={lexeme.definition}
+        onChange={change}
+        aria-label={`Gloss for lexeme ${index}`}
+      />
+      <div className={styles.clearButton}>
+        <Button
+          id={`lc${index}`}
+          onClick={change}
+          ver='danger'
+          small
+          type='button'
+          aria-label={`Delete lexeme ${index}`}
+        >
+          Delete
+        </Button>
+      </div>
+    </div>
+  ))
+
+  const derivationRows = data.derivations.map((derivation, index) => (
+    <div key={index} className={styles.derivRow}>
+      <input
+        type='text'
+        id={`da${index}`}
+        name={`da${index}`}
+        className={styles.derivationInput}
+        value={derivation.derivation}
+        onChange={change}
+        aria-label={`Affix ${index}`}
+      />
+      <input
+        type='text'
+        id={`dl${index}`}
+        name={`dl${index}`}
+        className={styles.derivationGlossInput}
+        value={derivation.gloss}
+        onChange={change}
+        aria-label={`Gloss for affix ${index}`}
+      />
+      <input
+        type='text'
+        id={`dd${index}`}
+        name={`dd${index}`}
+        className={styles.derivationDefInput}
+        value={derivation.definition}
+        onChange={change}
+        aria-label={`Definition for affix ${index}`}
+      />
+      <div className={styles.clearButton}>
+        <Button
+          id={`dc${index}`}
+          onClick={change}
+          ver='danger'
+          small
+          type='button'
+          aria-label={`Delete lexeme ${index}`}
+        >
+          Delete
+        </Button>
+      </div>
+    </div>
+  ))
+
+  const addButton = which => (
+    <div className={styles.addingRow}>
+      <Button
+        onClick={change}
+        id={`${which}Add`}
+        ver='success'
+        small
+        type='button'
+      >
+        Add
+      </Button>
+    </div>
+  )
+
+  const invokeFilePicker = e => {
+    e.preventDefault()
+    filePicker.current.value = ''
+    filePicker.current.click()
+  }
+
+  return (
+    <Form name='deriv-form'>
+      <div className={classNames(styles.lexemes, sharedStyles.part)}>
+        <h5 className={sharedStyles.sectionTitle}>Lexemes</h5>
+        <div className={styles.derivRow}>
+          <h6 className={classNames(styles.columnLabel, styles.wordLabel)}>
+            Word
+          </h6>
+          <h6
+            className={classNames(styles.columnLabel, styles.definitionLabel)}
+          >
+            Gloss
+          </h6>
+        </div>
+        {lexemeRows}
+        {addButton('lex')}
+      </div>
+      <div className={classNames(styles.derivations, sharedStyles.part)}>
+        <h5 className={sharedStyles.sectionTitle}>Derivational Affixes</h5>
+        <div className={styles.derivRow}>
+          <h6 className={classNames(styles.columnLabel, styles.affixLabel)}>
+            Affix
+          </h6>
+          <h6 className={classNames(styles.columnLabel, styles.glossLabel)}>
+            Gloss
+          </h6>
+          <h6
+            className={classNames(styles.columnLabel, styles.definitionLabel)}
+          >
+            Definition
+          </h6>
+        </div>
+        {derivationRows}
+        {addButton('der')}
+      </div>
+      <Control addedClasses={sharedStyles.part}>
+        <ControlSide side='left'>
+          <ControlPiece>
+            <Button type='submit' ver='neutral' onClick={change}>
+              Derive
+            </Button>
+          </ControlPiece>
+          <ControlPiece>
+            <label htmlFor='words'>number of words to derive:</label>
+            <input
+              type='number'
+              id='words'
+              name='words'
+              min='1'
+              max='9999'
+              className={styles.wordsInput}
+              value={data.words}
+              onChange={change}
+            />
+          </ControlPiece>
+          <ControlPiece>
+            <label htmlFor='type'>output type:</label>
+            <select id='type' name='type' value={data.type} onChange={change}>
+              <option value='any'>any base, any derivation</option>
+              <option value='single base'>single base</option>
+              <option value='single derivation'>single derivation</option>
+            </select>
+          </ControlPiece>
+        </ControlSide>
+        <ControlSide side='right'>
+          <ControlPiece>
+            <Button id='save' ver='success' onClick={change} type='button'>
+              Save
+            </Button>
+          </ControlPiece>
+          <ControlPiece>
+            <Button id='open' onClick={invokeFilePicker} type='button'>
+              Open
+            </Button>
+            <input
+              id='file'
+              name='file'
+              className={sharedStyles.hidden}
+              type='file'
+              ref={filePicker}
+              onChange={change}
+            />
+          </ControlPiece>
+        </ControlSide>
+      </Control>
+    </Form>
+  )
+}
 
 DerivForm.propTypes = {
-  styles: PropTypes.string
+  styles: PropTypes.object,
+  data: PropTypes.shape({
+    words: PropTypes.number.isRequired,
+    type: PropTypes.oneOf(['any', 'single base', 'single derivation'])
+      .isRequired,
+    lexemes: PropTypes.arrayOf(
+      PropTypes.shape({
+        lexeme: PropTypes.string.isRequired,
+        definition: PropTypes.string.isRequired
+      })
+    ),
+    derivations: PropTypes.arrayOf(
+      PropTypes.shape({
+        derivation: PropTypes.string.isRequired,
+        gloss: PropTypes.string.isRequired,
+        definition: PropTypes.string.isRequired
+      })
+    )
+  }).isRequired,
+  change: PropTypes.func.isRequired
 }
 
 export default DerivForm
--- src/pages/deriv/index.js
+++ src/pages/deriv/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 
-import Notice from '../../components/notice'
 import Tool from '../../components/tool'
 
 import DerivForm from '../../pageComponents/deriv/derivForm'
 import DerivResults from '../../pageComponents/deriv/derivResults'
-import DerivService from '../../services/derivService'
+import derivService from '../../services/derivService'
 
 import styles from './deriv.module.sass'
 
@@ -30,11 +29,109 @@ const DerivPage = () => {
   const metadata = data.site.siteMetadata
   const toolInfo = metadata.toolInfo.deriv
 
+  // State
+
+  const [words, setWords] = useState(derivService.getData().words)
+  const [type, setType] = useState(derivService.getData().type)
+  const [lexemes, setLexemes] = useState(derivService.getData().lexemes)
+  const [derivations, setDerivations] = useState(
+    derivService.getData().derivations
+  )
+  const [results, setResults] = useState(derivService.getData().results)
+
+  // Functions
+
+  const onChange = e => {
+    const state = { words, type, lexemes, derivations, results }
+    if (e.target.tagName === 'BUTTON') {
+      e.preventDefault()
+      if (e.target.type === 'submit') {
+        // Derive the output
+        const response = derivService.derive(state)
+        state.results = response
+        setResults(response)
+        // Save the current state to storage
+        derivService.setStorage(state)
+      } else if (e.target.id === 'save') {
+        // Save the current state to storage and generate a file
+        derivService.save(state)
+      } else if (e.target.id.slice(3) === 'Add') {
+        const which = e.target.id.slice(0, 3)
+        // When the add button is clicked, add a blank row to state
+        const newData = derivService.add(state, which)
+        setLexemes(newData.lexemes)
+        setDerivations(newData.derivations)
+      } else if (e.target.id.slice(1, 2) === 'c') {
+        // When a row is cleared, delete teh corresponding row from state
+        const idNum = e.target.id.slice(2)
+        const which = e.target.id.slice(0, 1)
+        const newData = derivService.clear(state, idNum, which)
+        setLexemes(newData.lexemes)
+        setDerivations(newData.derivations)
+      }
+    } else if (e.target.type === 'file') {
+      e.preventDefault()
+      // Open a file and parse it to restore saved state
+      const file = e.target.files[0]
+      const updateState = response => {
+        if (response) {
+          // Only change state if the file was successfully opened
+          setWords(response.words)
+          setType(response.type)
+          setLexemes(response.lexemes)
+          setDerivations(response.derivations)
+          setResults(response.results)
+        }
+      }
+
+      derivService.open(file, updateState)
+    } else {
+      const idNum = e.target.id.slice(2)
+      const which = e.target.id.slice(0, 2)
+      const val = e.target.value
+      const name = e.target.name
+      if (
+        which === 'lw' ||
+        which === 'ld' ||
+        which === 'da' ||
+        which === 'dl' ||
+        which === 'dd'
+      ) {
+        const response = derivService.changeInput(state, idNum, which, val)
+        if (response) {
+          // Only change state if the input was successfully changed
+          setWords(response.words)
+          setType(response.type)
+          setLexemes(response.lexemes)
+          setDerivations(response.derivations)
+          setResults(response.results)
+        }
+      } else if (e.target.name === 'words') {
+        const response = derivService.wordNumChange(state, val)
+        if (response) {
+          // Only change state if the word number was successfully changed
+          setWords(response.words)
+          setType(response.type)
+          setLexemes(response.lexemes)
+          setDerivations(response.derivations)
+          setResults(response.results)
+        }
+      } else {
+        if (e.target.name === 'type') {
+          setType(val)
+        }
+      }
+    }
+  }
+
   return (
     <Tool toolInfo={toolInfo}>
-      <Notice>This tool is still in development.</Notice>
-      <DerivForm />
-      <DerivResults />
+      <DerivForm
+        styles={styles}
+        data={{ words, type, lexemes, derivations }}
+        change={onChange}
+      />
+      <DerivResults styles={styles} results={results} />
     </Tool>
   )
 }
