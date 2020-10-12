b595e9f13c7f6c15e4e9835c56986ef493be9332
Ian Cook <6866411+nai888@users.noreply.github.com>
2020-10-11 20:13:31 -0500

    Add offline PWA support, fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,17 +66,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Langua`,
+        short_name: `Langua`,
         start_url: `/langua`,
-        background_color: `#21646e`,
-        theme_color: `#21646e`,
+        background_color: `#2f8f9d`,
+        theme_color: `#2f8f9d`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.svg` // This path is relative to the root of the site.
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ]
 }
